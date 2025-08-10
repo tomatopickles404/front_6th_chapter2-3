e1133bc7e592a3e5d5eb769b8fb9e8b98a1216eb
xxziiko <xxziiko@gmail.com>
2025-08-10 23:59:11 +0900

    fix: alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import viteTsconfigPaths from "vite-tsconfig-paths"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  resolve: {
+    alias: {
+      "@/*": path.resolve(__dirname, "src/*"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
